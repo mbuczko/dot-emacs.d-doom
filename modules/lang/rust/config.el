b88259d5a708aaf630eec09fb601be0b9b645ca8
Michal Buczko <michal.buczko@gmail.com>
2020-09-11 16:35:32 +0200

    rust config improved

--- modules/lang/rust/config.el
+++ modules/lang/rust/config.el
@@ -10,11 +10,23 @@
   (after! org-src
     (add-to-list 'org-src-lang-modes '("rust" . rustic)))
   :config
+  (require 'helm-lsp)
+  (define-key rustic-mode-map (kbd "C-x C-d") #'lsp-describe-thing-at-point)
+  (define-key rustic-mode-map (kbd "C-c n")   #'helm-lsp-workspace-symbol)
+  (define-key rustic-mode-map (kbd "C-c c")   #'helm-lsp-code-actions)
   (setq rustic-lsp-server (quote rust-analyzer)
         rustic-lsp-client (quote lsp-mode)
         rustic-lsp-format t)
-
-  (define-key rustic-mode-map (kbd "C-x C-d") #'lsp-describe-thing-at-point)
-
   (after! rustic-flycheck
     (add-to-list 'flycheck-checkers 'rustic-clippy)))
+
+(use-package lsp-completion)
+
+(use-package lsp-modeline
+  :commands (lsp-modeline-diagnostics-mode lsp-modeline-code-actions-mode))
+
+(use-package lsp-ui
+  :mode ("\\.rs$" . rustic-mode)
+  :commands (lsp-ui-sideline-mode)
+  :config
+  (lsp-ui-sideline-mode t))
